57a2351eb1d586b0617817e24c1d56134e776ecd
DmitryKolotilshikov <it_proger@mail.ru>
2020-03-03 23:32:24 +0300

    v3_modal

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+let fruits = [
+    {id: 1, title: 'Apple', price: 39, img: 'https://felomena.com/wp-content/images/sonnik/bukva/ya/yabloko.jpg'},
+    {id: 2, title: 'Orange', price: 19, img: 'https://m.dom-eda.com/uploads/images/catalog/item/dfc9a3e974/3cbf3bd41c_1000.jpg'},
+    {id: 3, title: 'Apricot', price: 49, img: 'https://misterposter.ru/image/cache/data/wallpaper/abrikos_Ob5881_141813925-764x764.jpg'}
+]
+
 const modal = $.modal({
     title: 'Dmitry Kolotilshikov',
     closable: true,
@@ -17,3 +23,26 @@ const modal = $.modal({
         } }    
     ]
 });
+
+const toHTML = fruit => `
+<div class="col">
+    <div class="card">
+        <img class="card-img-top" style="height: 300px" src="${fruit.img}" alt="${fruit.title}">
+        <div class="card-body">
+        <h5 class="card-title">${fruit.title}</h5>
+        <a href="#" class="btn btn-primary" data-get="price">Get Price</a>
+        <a href="#" class="btn btn-danger">Delete</a>
+        </div>
+    </div>               
+</div>  
+`
+const render = function() {
+    const html = fruits.map(fruit => toHTML(fruit)).join('');
+    document.querySelector('#fruits').innerHTML = html;
+
+
+}
+render();
+
+
+
